521ad4db04409861cbc9bf67769ebaa9fcc7b7f1
nullworkbench <nullworkbench@gmail.com>
2020-10-21 22:35:03 +0900

    textareaが改行に応じて高さ変わるように

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import firebase, { db } from "../plugins/firebase";
 
 import Head from "next/head";
-import styles from "../styles/Home.module.css";
+import styles from "../styles/index.module.scss";
 
 class Index extends React.Component {
   constructor(props) {
@@ -12,24 +12,6 @@ class Index extends React.Component {
       posts: [],
     };
   }
-
-  // async getPosts() {
-  //   const items = [];
-  //   await db
-  //     .collection("posts")
-  //     .orderBy("createdAt", "desc")
-  //     .get()
-  //     .then((docs) => {
-  //       docs.forEach((doc) => {
-  //         let item = doc.data();
-  //         item.id = doc.id;
-  //         items.push(item);
-  //       });
-  //       this.setState({ posts: items }); // this.state.posts.pushはダメなのでsetStateで
-  //     });
-  //   console.log(this.state.posts);
-  //   console.log(this.state.posts.length);
-  // }
 
   onCollectionUpdate = (querySnapshot) => {
     const items = [];
@@ -71,7 +53,8 @@ class Index extends React.Component {
   }
 
   async handleUpdate(docId) {
-    const title = document.getElementById("renew_post_title").value;
+    const title = document.getElementById("renew_post_title_" + docId).value;
+    document.getElementById("renew_post_title_" + docId).value = "";
     const res = await db
       .collection("posts")
       .doc(docId)
@@ -84,27 +67,16 @@ class Index extends React.Component {
         // error
         console.log(error);
       });
-    console.log(res);
+  }
+
+  execTextareaHeight() {
+    const textarea = document.getElementById("new_post_body");
+    const lines = (textarea.value + "\n").match(/\n/g).length;
+    textarea.style.height = lines + "rem";
   }
 
   async componentDidMount() {
-    // await this.getPosts();
-
-    // db.collection("posts").onSnapshot((snapshot) => {
-    //   snapshot.docChanges().forEach((change) => {
-    //     if (change.type === "added") {
-    //       console.log("added");
-    //       this.getPosts();
-    //     } else if (change.type === "modified") {
-    //       console.log("modified");
-    //       this.getPosts();
-    //     } else if (change.type === "removed") {
-    //       console.log("removed");
-    //       this.getPosts();
-    //     }
-    //   });
-    // });
-
+    // posts取得・リアルタイム更新
     this.unsubscribe = db
       .collection("posts")
       .onSnapshot(this.onCollectionUpdate);
@@ -117,18 +89,23 @@ class Index extends React.Component {
   }
 
   componentWillUnmount() {
+    //posts 監視終了
     this.unsubscribe();
-    // db.collection("posts").onSnapshot(() => {});
   }
 
   render() {
     return (
-      <div className={styles.container}>
+      <div>
         <Head>
           <title>Flash</title>
           <link rel="icon" href="/favicon.ico" />
         </Head>
 
+        <header className={styles.header}>
+          <h1>Flash</h1>
+        </header>
+
+        {/* LOGIN START */}
         <div>
           {this.state.user ? (
             <button onClick={this.logout}>Logout</button>
@@ -136,26 +113,29 @@ class Index extends React.Component {
             <button onClick={this.login}>Login</button>
           )}
         </div>
+        {/* LOGIN END */}
 
-        <div id="new_post">
+        {/* 新規投稿 START */}
+        <div id="new_post" className={styles.new_post}>
           <form onSubmit={this.handlePost}>
             <input type="text" id="new_post_title" placeholder="title" />
-            <textarea id="new_post_body" cols="30" rows="10"></textarea>
+            <textarea id="new_post_body" onChange={this.execTextareaHeight} />
             <button type="submit">POST</button>
           </form>
         </div>
+        {/* 新規投稿 END */}
 
+        {/* 投稿一覧 START */}
         <div id="posts">
           {this.state.posts.map((post) => (
             <div key={post.id}>
               <h3>{post.title}</h3>
-              <span>{post.id}</span>
               <button onClick={() => this.handleDelete(post.id)}>DELETE</button>
               {/* onClick={this.handleDelete(post.id)} とすると {}内がプログラムとして認識され、handleDelete()が発火してしまう */}
               <div id="renew">
                 <input
                   type="text"
-                  id="renew_post_title"
+                  id={"renew_post_title_" + post.id}
                   placeholder={post.title}
                 />
                 <button onClick={() => this.handleUpdate(post.id)}>
@@ -165,6 +145,7 @@ class Index extends React.Component {
             </div>
           ))}
         </div>
+        {/* 投稿一覧 END */}
       </div>
     );
   }
