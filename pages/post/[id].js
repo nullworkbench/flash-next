2bdd0d4c879c2d910d6d5d7e467f73c2c01835d4
nullworkbench <nullworkbench@gmail.com>
2020-11-24 22:13:28 +0900

    detailページでhighlight.jsに対応

--- pages/post/[id].js
+++ pages/post/[id].js
@@ -1,14 +1,13 @@
 import firebase, { db } from "../../plugins/firebase";
+import highlightjs from "../../components/highlight";
+
 import React from "react";
 class MessageBox extends React.Component {
   constructor() {
     super();
-    console.log("Called constructor");
 
     //初期値
-    this.state = {
-      posts: [{ id: 0, title: "Never Ending Story" }],
-    };
+    this.state = {};
   }
 
   static async getInitialProps() {
@@ -30,45 +29,31 @@ class MessageBox extends React.Component {
     return { post: result };
   }
 
-  // componentDidMount() {
-  //   console.log("Called componentDidMount");
+  componentDidMount() {
+    console.log("Called componentDidMount");
 
-  //   const items = [];
-  //   db.collection("posts")
-  //     .doc("pLJDgb5B7fy7IK0yL6bS")
-  //     .get()
-  //     .then((doc) => {
-  //       const { title, body } = doc.data();
+    let obj = this.props.post.body;
+    // 全ての@@@を置き換える
+    while (obj.match(/@@@/)) {
+      if (obj.match(/@@@\n/)) {
+        obj = obj.replace(/@@@\n/, "<pre><code>");
+      } else {
+        obj = obj.replace("@@@", "<pre><code>");
+      }
+      obj = obj.replace("@@@", "</code></pre>");
+    }
 
-  //       items.push({
-  //         id: doc.id,
-  //         title: title,
-  //         body: body,
-  //       });
-  //     });
-
-  //   // //state を更新(した気になる...がしかし)
-  //   this.setState({ posts: items });
-
-  //   // console.log("Called setState in componentDidMount");
-
-  //   // //state が変わってない！！
-  //   console.log(this.state.posts); // => "なんでもしますから"
-  // }
+    const target = document.getElementById("post_body");
+    target.insertAdjacentHTML("afterbegin", obj);
+    highlightjs();
+  }
 
   render() {
-    console.log("Called render");
-
     const post = this.props.post;
-    console.log(this.props.post);
-    //ここでようやく setState で予約した state の変更を確認
-    console.log(this.state.posts);
-    // console.log(JSON.stringify(this.state.posts)); //=> "なんでもするとは言ってない"
-    // console.log(this.state.posts.length);
-    // console.log(Array.from(this.state.posts));
     return (
       <div>
         <h2>{post.title}</h2>
+        <div id="post_body"></div>
       </div>
     );
   }
