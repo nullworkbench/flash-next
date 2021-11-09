3ba2ee7ec0ca1e0871b4de7cd7f0706141b7e38f
nullworkbench <nullworkbench@gmail.com>
2021-11-09 13:28:32 +0900

    highlight.jsコンポーネント作成

---
+++ components/CodeArea.tsx
@@ -0,0 +1,21 @@
+import { FC, useEffect } from "react";
+// highlight.js
+import hljs from "highlight.js";
+// highlight.jsのテーマ指定
+import "highlight.js/styles/vs2015.css";
+
+const CodeArea: FC = ({ children }) => {
+  // mounted時にhljs実行s
+  useEffect(() => {
+    hljs.highlightAll();
+  }, []);
+  return (
+    <div>
+      <pre>
+        <code className="font-code">{children}</code>
+      </pre>
+    </div>
+  );
+};
+
+export default CodeArea;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,13 @@
 import type { NextPage } from "next";
 import styles from "../styles/Home.module.css";
+import CodeArea from "@/components/CodeArea";
 
 const Home: NextPage = () => {
-  return <div className={styles.container}></div>;
+  return (
+    <div className={styles.container}>
+      <CodeArea>display: block;</CodeArea>
+    </div>
+  );
 };
 
 export default Home;
