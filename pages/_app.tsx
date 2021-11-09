28c8f70e319cedcf0b96b3a9f9faee2ad86acd58
nullworkbench <nullworkbench@gmail.com>
2021-11-09 11:40:35 +0900

    tailwind setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,12 @@
-import "../styles/globals.css";
+import "tailwindcss/tailwind.css";
+import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
 function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <div className="font-jp">
+      <Component {...pageProps} />
+    </div>
+  );
 }
 export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,37 @@
+module.exports = {
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      screens: {
+        sp: { max: "559px" },
+        tab: { min: "560px", max: "959px" },
+        pc: { min: "960px" },
+      },
+      zIndex: {
+        99: 99,
+      },
+    },
+    fontFamily: {
+      jp: [
+        "-apple-system",
+        "BlinkMacSystemFont",
+        "ヒラギノ角ゴシック",
+        "Hiragino Sans",
+        "YuGothic",
+        "Yu Gothic",
+        "Source Sans Pro",
+        "Segoe UI",
+        "Roboto",
+        "Helvetica Neue",
+        "Arial",
+        "sans-serif",
+      ],
+      code: ["Courier New", "Courier", "monospace"],
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
