2e98f06bd48caaec8ec08c37d43f9aff6e7d7c65
nullworkbench <nullworkbench@gmail.com>
2021-11-25 18:36:37 +0900

    firebaseの環境変数にを付与しFirestoreへ書き込みができるように

--- plugins/firebase.ts
+++ plugins/firebase.ts
@@ -8,14 +8,15 @@ import {
 } from "firebase/auth";
 
 const config = {
+  // NEXT_PUBLIC_を先頭につけないと、Next.js Server側でしか環境変数を読み込んでくれない
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.FIREBASE_DB_URL,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
-  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DB_URL,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FNEXT_PUBLIC_IREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // firebase init
