801e14b76c07175032433a200c9d76de190a7a72
nullworkbench <nullworkbench@gmail.com>
2021-11-25 23:29:05 +0900

    コードのCSSを調整

--- src/components/CodeArea.tsx
+++ src/components/CodeArea.tsx
@@ -5,13 +5,15 @@ import hljs from "highlight.js";
 import "highlight.js/styles/vs2015.css";
 
 const CodeArea: FC = ({ children }) => {
-  // mounted時にhljs実行s
+  // mounted時にhljs実行
   useEffect(() => {
     hljs.highlightAll();
   }, []);
   return (
-    <pre>
-      <code className="font-code">{children}</code>
+    <pre className="py-2">
+      <code className="font-code rounded-md" style={{ padding: "1.5rem 2rem" }}>
+        {children}
+      </code>
     </pre>
   );
 };
