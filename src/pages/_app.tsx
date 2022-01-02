e391125ec569d10d584cc6e1218196a2a049a15b
nullworkbench <nullworkbench@gmail.com>
2022-01-02 18:41:55 +0900

    クローラーを拒否するように

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,21 @@
 import "tailwindcss/tailwind.css";
 import "@/styles/global.scss";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 import GlobalHeader from "@/components/GlobalHeader";
 
 function App({ Component, pageProps }: AppProps) {
   return (
-    <div className="font-jp">
-      <GlobalHeader />
-      <Component {...pageProps} />
-    </div>
+    <>
+      <Head>
+        {/* クローラー拒否 */}
+        <meta name="robots" content="noindex" />
+      </Head>
+      <div className="font-jp">
+        <GlobalHeader />
+        <Component {...pageProps} />
+      </div>
+    </>
   );
 }
 export default App;
