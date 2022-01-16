27964ff6eed1dea74b9d810504038dc040b18e1d
nullworkbench <nullworkbench@gmail.com>
2022-01-16 18:02:20 +0900

    CodeAreaのスタイルを調整

--- src/components/CodeArea.tsx
+++ src/components/CodeArea.tsx
@@ -21,7 +21,7 @@ const CodeArea: FC<Props> = ({ code }: Props) => {
   }, []);
   return (
     <pre className="relative py-2">
-      <span className="absolute top-5 left-4 px-3 text-sm text-black bg-gray-300 rounded-md">
+      <span className="absolute top-5 left-3.5 px-3 text-sm text-black bg-gray-300 rounded-md">
         {language == "bash" ? "" : language}
       </span>
       <code
