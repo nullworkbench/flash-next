5384d805c6909dca070f442051cd9a4ad91fa689
nullworkbench <nullworkbench@gmail.com>
2022-01-16 14:13:50 +0900

    Aboutページを作成

---
+++ src/pages/about.tsx
@@ -0,0 +1,11 @@
+import { NextPage } from "next";
+
+const About: NextPage = () => {
+  return (
+    <div className="container mx-auto">
+      <h2>Flashとは</h2>
+    </div>
+  );
+};
+
+export default About;
