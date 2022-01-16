bbcc8b5a5871c4f744ddab061d92aff47543965b
nullworkbench <nullworkbench@gmail.com>
2022-01-16 13:30:23 +0900

    PopupMenuに削除確認モーダルを追加

--- src/components/PopupMenu.tsx
+++ src/components/PopupMenu.tsx
@@ -1,5 +1,6 @@
 import { Children, useState } from "react";
 import Router from "next/router";
+import styled from "styled-components";
 import Icon from "./Icon";
 import { deletePost } from "@/plugins/firestore";
 import Modal from "@/components/Modal";
@@ -9,14 +10,18 @@ type Props = {
 };
 
 const PopupMenu: React.FC<Props> = ({ docId }) => {
+  // Popupメニューの開閉状態
   const [isOpen, setIsOpen] = useState(false);
 
+  // 削除確認モーダルの開閉状態
   const [deleteModalIsOpen, setDeleteModalIsOpen] = useState(false);
 
-  const MenuItem: React.FC = ({ children }) => {
-    return <div className="px-2 py-1">{children}</div>;
-  };
+  // Menuの各ボタンのスタイル
+  const MenuItem = styled.div`
+    padding: 0.5rem 1rem;
+  `;
 
+  // 削除ボタンが選択されたとき
   async function deleteBtn() {
     const res = await deletePost(docId);
     Router.reload();
@@ -41,16 +46,30 @@ const PopupMenu: React.FC<Props> = ({ docId }) => {
         <div className="text-right px-2 py-1" onClick={() => setIsOpen(false)}>
           &times;
         </div>
-        <div onClick={() => deleteBtn()}>
-          <MenuItem>Delete</MenuItem>
-        </div>
+        <MenuItem onClick={() => setDeleteModalIsOpen(true)}>Delete</MenuItem>
       </div>
       {/* 削除確認モーダル */}
       <Modal
         isOpen={deleteModalIsOpen}
         closeModal={() => setDeleteModalIsOpen(false)}
       >
-        <p>a</p>
+        <div className="p-3">
+          <p>本当に削除しますか？</p>
+        </div>
+        <div className="flex justify-end mt-4">
+          <button
+            className="px-4 py-2 mr-4 text-sm bg-gray-300 rounded-md focus:outline-none"
+            onClick={() => setDeleteModalIsOpen(false)}
+          >
+            キャンセル
+          </button>
+          <button
+            className="px-4 py-2 text-white text-sm bg-red-400 rounded-md focus:outline-none"
+            onClick={() => deleteBtn()}
+          >
+            削除する
+          </button>
+        </div>
       </Modal>
     </div>
   );
